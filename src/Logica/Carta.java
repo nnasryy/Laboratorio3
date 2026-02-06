5967b11b2131be13009f9ea05e2ef40602ab7742
nnasryy <nasrywilliamsbali@gmail.com>
2026-02-06 08:59:09 -0600

    Carta.java

--- src/Logica/Carta.java
+++ src/Logica/Carta.java
@@ -8,6 +8,30 @@ package Logica;
  *
  * @author nasry
  */
-public class Carta {
-    
+
+
+import javax.swing.*;
+
+public abstract class Carta {
+
+    protected boolean descubierta;
+    protected String id;
+    protected ImageIcon imagen;
+
+    public Carta(String id, ImageIcon imagen) {
+        this.id = id;
+        this.imagen = imagen;
+        this.descubierta = false;
+    }
+
+    public abstract void mostrar(JButton boton);
+    public abstract void ocultar(JButton boton);
+
+    public boolean estaDescubierta() {
+        return descubierta;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
